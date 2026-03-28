29641137ca35215dbdbb6e27481513ce02b56cec
abbey <61197745+affectioned@users.noreply.github.com>
2026-03-28 23:51:57 +0100

    Remove Behaviour.cs

--- src/Tarkov/Unity/Behaviour.cs
+++
@@ -1,9 +0,0 @@
-﻿
-namespace eft_dma_radar.Common.Unity.LowLevel
-{
-    public readonly struct Behaviour
-    {
-        public static implicit operator ulong(Behaviour x) => x.Base;
-        private readonly ulong Base;
-    }
-}
